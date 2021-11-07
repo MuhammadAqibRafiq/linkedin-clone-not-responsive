5463314a7f7142e766811cfd1aecfec29449f64e
MuhammadAqibRafiq <77021708+MuhammadAqibRafiq@users.noreply.github.com>
2021-11-08 00:51:59 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAb0p2l9Ntf3AhFppAa7DpvUIGl9kqHsvU",
-    authDomain: "diary-dc615.firebaseapp.com",
-    projectId: "diary-dc615",
-    storageBucket: "diary-dc615.appspot.com",
-    messagingSenderId: "659105278765",
-    appId: "1:659105278765:web:d11b8deab0428b8f1901a4"
+    apiKey: ***************,
+    authDomain: ***************,
+    projectId: ***************,
+    storageBucket: ***************,
+    messagingSenderId: ***************,
+    appId: ***************
 };
 firebase.initializeApp(firebaseConfig)
 
